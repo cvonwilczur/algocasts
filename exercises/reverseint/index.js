db702bec9150b4d1c873df68d56221546af19df7
cvonwilczur <cvonwilczur@gmail.com>
2018-10-11 20:49:53 -0700

    reverse integer solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // turn number into string
+  let numberString = n.toString();
+  let reversed = '';
+  //use a for loop to reverse order of string
+  for (let char of numberString) {
+    console.log(char)
+
+    reversed = char + reversed;
+  }
+  // use conditional to check if n was negative and add -
+  // use parseInt to convert back into a number
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
