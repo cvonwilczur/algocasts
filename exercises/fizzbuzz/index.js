62e5eea12548cc81fb74c33c6e5fb790d5a8d441
cvonwilczur <cvonwilczur@gmail.com>
2018-10-12 20:11:48 -0700

    fizzbuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for(let i = 1; i <= n; i++){
+    // if i is a multiple of 3 and 5, print fizzBuzz
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log('fizzbuzz');
+    } else if ( i % 3 === 0 ){
+      console.log('fizz');
+    } else if ( i % 5 === 0){
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+    // if i is a multiple of 3, print fizz
+    // if i is a multiple of 5, print buzz
+  }
+}
 
 module.exports = fizzBuzz;
